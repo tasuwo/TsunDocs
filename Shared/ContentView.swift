30140a733e331d793345d8d12a50264036b8c1d3
tasuwo <kamuhata.you@gmail.com>
2021-06-18 19:30:28 +0900

    :tada: Initial commit.

---
+++ Shared/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  Copyright © 2021 Tasuku Tozawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TsunDocsApp.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2021 Tasuku Tozawa. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct TsunDocsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
