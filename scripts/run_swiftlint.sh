bde90f116f4252de0fc55b68addd0efbdc87019a
tasuwo <kamuhata.you@gmail.com>
2021-06-19 14:52:39 +0900

    :hammer: ビルド時にSwiftLintを実行する

---
+++ scripts/run_swiftlint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [[ ${CONFIGURATION} == "Debug" ]]; then
+  if [[ -f "${PROJECT_DIR}/Pods/SwiftLint/swiftlint" ]]; then
+    "${PROJECT_DIR}/Pods/SwiftLint/swiftlint"
+  else
+    echo "warning: SwiftLint is not installed. Run 'bundle exec pod install --repo-update' to install it."
+  fi
+fi
