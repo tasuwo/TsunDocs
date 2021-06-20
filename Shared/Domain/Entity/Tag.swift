cea765c7e915fa680baa693b74bb84f5def74f13
tasuwo <kamuhata.you@gmail.com>
2021-06-20 16:49:10 +0900

    :sparkles: タグ表示用のCellを追加する

---
+++ Shared/Domain/Entity/Tag.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright © 2021 Tasuku Tozawa. All rights reserved.
+//
+
+import Foundation
+
+struct Tag {
+    let id: UUID
+    let name: String
+}
+
+extension Tag: Identifiable {}
---
+++ Shared/View/Tag/TagCell.swift
@@ -0,0 +1,77 @@
+//
+//  Copyright © 2021 Tasuku Tozawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct TagCell: View {
+    // MARK: - Properties
+
+    let tag: Tag
+    @State var isSelected = false
+
+    // MARK: - View
+
+    var body: some View {
+        HStack(spacing: 0) {
+            if isSelected {
+                Image(systemName: "checkmark")
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.trailing, 2)
+                    .frame(width: 18, height: 18)
+                    .fixedSize()
+                    .foregroundColor(.white)
+            } else {
+                Text("#")
+                    .font(.system(size: 20))
+                    .padding(.all, 0)
+                    .frame(width: 18, height: 18)
+                    .fixedSize()
+            }
+
+            Text(tag.name)
+                .foregroundColor(isSelected ? .white : .primary)
+                .font(.system(size: 16))
+        }
+        .padding([.leading, .trailing], 16)
+        .padding([.top, .bottom], 8)
+        .background(GeometryReader { geometry in
+            let baseView = isSelected
+                ? Color.green
+                : Color("secondary_background")
+            baseView
+                .clipShape(RoundedRectangle(cornerRadius: geometry.size.height / 2,
+                                            style: .continuous))
+        })
+        .overlay(GeometryReader { geometry in
+            if isSelected {
+                Color.clear
+            } else {
+                RoundedRectangle(cornerRadius: geometry.size.height / 2,
+                                 style: .continuous)
+                    .stroke(lineWidth: 0.5)
+                    .foregroundColor(Color("tag_separator"))
+            }
+        })
+    }
+}
+
+// MARK: - Preview
+
+struct TagCell_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            VStack {
+                TagCell(tag: .init(id: UUID(), name: "タグ"), isSelected: false)
+                TagCell(tag: .init(id: UUID(), name: "my tag"), isSelected: true)
+            }
+            .preferredColorScheme(.light)
+
+            VStack {
+                TagCell(tag: .init(id: UUID(), name: "タグ"), isSelected: false)
+                TagCell(tag: .init(id: UUID(), name: "my tag"), isSelected: true)
+            }
+            .preferredColorScheme(.dark)
+        }
+    }
+}
