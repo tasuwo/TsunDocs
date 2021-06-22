4a6fa6948b7241a4a7f0237588864cd029593f83
tasuwo <kamuhata.you@gmail.com>
2021-06-22 21:39:28 +0900

    :construction: サイドバー/タブバーを追加する

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -5,14 +5,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var idiom: UIUserInterfaceIdiom { UIDevice.current.userInterfaceIdiom }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if idiom == .pad {
+            NavigationView {
+                List {
+                    NavigationLink(destination: Text("TODO: TsundocList")) {
+                        TabItem.tsundocList.label
+                    }
+                    NavigationLink(destination: Text("TODO: SettingView")) {
+                        TabItem.settings.label
+                    }
+                }
+                .navigationTitle(LocalizedStringKey("app_name"))
+                .listStyle(SidebarListStyle())
+
+                Text("TODO: TsundocList")
+            }
+        } else {
+            TabView {
+                Text("TODO: TsundocList")
+                    .tabItem { TabItem.tsundocList.view }
+
+                Text("TODO: SettingView")
+                    .tabItem { TabItem.settings.view }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone 12")
+
+            ContentView()
+                .previewDevice("iPad Pro (12.9-inch) (3rd generation)")
+        }
     }
 }
