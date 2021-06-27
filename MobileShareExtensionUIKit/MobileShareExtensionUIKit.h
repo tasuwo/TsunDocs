6be986820ce5d223942332c4aa5f2394627b9e60
tasuwo <kamuhata.you@gmail.com>
2021-06-27 16:43:23 +0900

    :heavy_plus_sign: MobileShareExtensionUIKitを追加

---
+++ MobileShareExtensionUIKit/MobileShareExtensionUIKit.h
@@ -0,0 +1,15 @@
+//
+//  Copyright © 2021 Tasuku Tozawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MobileShareExtensionUIKit.
+FOUNDATION_EXPORT double MobileShareExtensionUIKitVersionNumber;
+
+//! Project version string for MobileShareExtensionUIKit.
+FOUNDATION_EXPORT const unsigned char MobileShareExtensionUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileShareExtensionUIKit/PublicHeader.h>
+
+
