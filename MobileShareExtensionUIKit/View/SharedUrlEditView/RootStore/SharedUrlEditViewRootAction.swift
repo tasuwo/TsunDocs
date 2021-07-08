36b19d7f37214e6ddea030df6d2fa8e0a68e1e9e
tasuwo <kamuhata.you@gmail.com>
2021-07-08 23:33:55 +0900

    :recycle: 状態を共有する

--- MobileShareExtensionUIKit/View/SharedUrlEditView/RootStore/SharedUrlEditViewRootAction.swift
+++ MobileShareExtensionUIKit/View/SharedUrlEditView/RootStore/SharedUrlEditViewRootAction.swift
@@ -13,28 +13,12 @@ extension SharedUrlEditViewRootAction {
     static let mappingToEdit: ActionMapping<Self, SharedUrlEditViewAction> = .init(build: {
         .edit($0)
     }, get: {
-        switch $0 {
-        case let .edit(action):
-            return action
-        case let .image(.selectedEmoji(emoji)):
-            return .onSelectedEmoji(emoji)
-        case .image(.didTapDeleteEmoji):
-            return .onSelectedEmoji(nil)
-        default:
-            return nil
-        }
+        guard case let .edit(action) = $0 else { return nil }; return action
     })
 
     static let mappingToImage: ActionMapping<Self, SharedUrlImageAction> = .init(build: {
         .image($0)
     }, get: {
-        switch $0 {
-        case let .image(action):
-            return action
-        case let .edit(.onLoad(_, meta)):
-            return .onLoadImageUrl(meta?.imageUrl)
-        default:
-            return nil
-        }
+        guard case let .image(action) = $0 else { return nil }; return action
     })
 }
--- MobileShareExtensionUIKit/View/SharedUrlEditView/RootStore/SharedUrlEditViewRootState.swift
+++ MobileShareExtensionUIKit/View/SharedUrlEditView/RootStore/SharedUrlEditViewRootState.swift
@@ -3,20 +3,68 @@
 //
 
 import CompositeKit
+import TsunDocsUIKit
 
 struct SharedUrlEditViewRootState: Equatable {
-    var edit: SharedUrlEditViewState
-    var image: SharedUrlImageState
+    var editViewState: SharedUrlEditViewState {
+        get {
+            .init(sharedUrl: sharedUrl,
+                  sharedUrlTitle: sharedUrlTitle,
+                  sharedUrlDescription: sharedUrlDescription,
+                  sharedUrlImageUrl: sharedUrlImageUrl,
+                  selectedEmoji: selectedEmoji,
+                  alert: alert)
+        }
+        set {
+            sharedUrl = newValue.sharedUrl
+            sharedUrlTitle = newValue.sharedUrlTitle
+            sharedUrlDescription = newValue.sharedUrlDescription
+            sharedUrlImageUrl = newValue.sharedUrlImageUrl
+            selectedEmoji = newValue.selectedEmoji
+            alert = newValue.alert
+        }
+    }
+
+    var imageState: SharedUrlImageState {
+        get {
+            .init(imageUrl: sharedUrlImageUrl,
+                  thumbnailLoadingStatus: thumbnailLoadingStatus,
+                  selectedEmoji: selectedEmoji,
+                  isSelectingEmoji: isSelectingEmoji)
+        }
+        set {
+            sharedUrlImageUrl = newValue.imageUrl
+            thumbnailLoadingStatus = newValue.thumbnailLoadingStatus
+            selectedEmoji = newValue.selectedEmoji
+            isSelectingEmoji = newValue.isSelectingEmoji
+        }
+    }
+
+    // MARK: - Shared
+
+    var sharedUrlImageUrl: URL?
+    var selectedEmoji: Emoji?
+
+    // MARK: - SharedUrlEditViewState
+
+    var sharedUrl: URL?
+    var sharedUrlTitle: String?
+    var sharedUrlDescription: String?
+    var alert: SharedUrlEditViewState.Alert?
+
+    // MARK: - SharedUrlImageState
+
+    var thumbnailLoadingStatus: AsyncImageStatus?
+    var isSelectingEmoji: Bool
 }
 
 extension SharedUrlEditViewRootState {
     init() {
-        self.edit = .init()
-        self.image = .init(imageUrl: nil)
+        isSelectingEmoji = false
     }
 }
 
 extension SharedUrlEditViewRootState {
-    static let mappingToEdit: StateMapping<Self, SharedUrlEditViewState> = .init(keyPath: \Self.edit)
-    static let mappingToImage: StateMapping<Self, SharedUrlImageState> = .init(keyPath: \Self.image)
+    static let mappingToEdit: StateMapping<Self, SharedUrlEditViewState> = .init(keyPath: \Self.editViewState)
+    static let mappingToImage: StateMapping<Self, SharedUrlImageState> = .init(keyPath: \Self.imageState)
 }
