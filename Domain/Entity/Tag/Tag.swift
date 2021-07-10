32d751acb729fc2ccb2ce3d78df6f095262adcc0
tasuwo <kamuhata.you@gmail.com>
2021-07-10 12:38:44 +0900

    :construction: タグの選択用Viewを追加する

--- Domain/Entity/Tag/Tag.swift
+++ Domain/Entity/Tag/Tag.swift
@@ -5,17 +5,22 @@
 import Foundation
 
 // sourcery: AutoDefaultValue
-public struct Tag {
+public struct Tag: Searchable {
     // MARK: - Properties
 
     public let id: UUID
     public let name: String
+
+    // MARK: - Searchable
+
+    public let searchableText: String?
 
     // MARK: - Initializers
 
     public init(id: UUID, name: String) {
         self.id = id
         self.name = name
+        self.searchableText = name.transformToSearchableText()
     }
 }
 
