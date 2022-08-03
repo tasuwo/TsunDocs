9383290c7cc35094e360179b7d4faab2fda73cdb
tasuwo <kamuhata.you@gmail.com>
2022-08-03 23:16:17 +0900

    router

---
+++ TsunDocsKit/Sources/Environment/Router/StackRouter.swift
@@ -0,0 +1,21 @@
+//
+//  Copyright ©︎ 2022 Tasuku Tozawa. All rights reserved.
+//
+
+import SwiftUI
+
+public class StackRouter: ObservableObject {
+    @Published var stack: NavigationPath = .init()
+
+    // MARK: - Initializers
+
+    public init() {}
+}
+
+extension StackRouter: Router {
+    // MARK: - Router
+
+    public func push(_ route: any Route) {
+        stack.append(route)
+    }
+}
